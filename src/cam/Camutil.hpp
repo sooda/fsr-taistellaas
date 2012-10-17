cc8e828bd60d68b55a9d83b797c45b7464147216
Toni Liski <toni.liski@aalto.fi>
2012-10-17 21:43:01 +0300

    cam: initial version of Camutil
    Create initial version of Camutil utility class for the Camera module
    Add it also into Makefile

---
+++ src/cam/Camutil.hpp
@@ -0,0 +1,31 @@
+#ifndef _J2B2_CAMERA_HPP_
+#define _J2B2_CAMERA_HPP_
+
+#include <MaCI/MachineCtrlClient.hpp>
+#include "../J2B2-API.hpp"
+#include "../SLAM/SLAMutil.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+/*
+ * Camera Utility class
+ * Toni Liski / 2012-10-17
+*/
+
+namespace cam {
+
+class Camutil {
+
+public:
+
+// constructor
+Camutil();
+
+static cv::Mat imgToMat (MaCI::Image::CImageContainer);
+static void FindBalls (cv::Mat);
+
+private:
+
+};
+}
+#endif

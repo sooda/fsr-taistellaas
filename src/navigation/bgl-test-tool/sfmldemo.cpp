3f9c4f1ad182358288eddb0602e7d7b43cf67ff8
Konsta 'sooda' Hölttä <konsta.holtta@aalto.fi>
2012-12-09 14:21:06 +0200

    a*: dilate walls before routing
    The search algorithm assumes that the robot is a dot. Dilate the map
    to create artificial walls to work around the physical size of the
    robot.

--- src/navigation/bgl-test-tool/sfmldemo.cpp
+++ src/navigation/bgl-test-tool/sfmldemo.cpp
@@ -9,9 +9,10 @@
 #include <stdexcept>
 #include "gridsearch.hpp"
 #include "getturns.hpp"
+#include "dilate.hpp"
 
 const int ww = 1024, wh = 768; // win wid, hei
-const int sz = 32;
+const int sz = 16;
 const int gw = ww/sz, gh = wh/sz; // grid size
 
 using namespace std;
@@ -27,11 +28,13 @@ int main()
 	vector<pair<vertex, vertex>> corners;
 	my_pred_map p;
 	sf::RenderWindow App(sf::VideoMode(ww, wh), "mouse buttons for walls, space for routing");
-	vector<vector<bool>> grid(gh, vector<bool>(gw));
+	GridMap grid(gh, GridRow(gw));
+	GridMap dilagrid(gh, GridRow(gw));
 	VectorGrid container(grid);
 
 	int lastx = 0;
 	int lasty = 0;
+	const int DILATION_AMOUNT = 3;
 	while (App.IsOpened())
 	{
 		// Process events
@@ -56,14 +59,16 @@ int main()
 		bool pressed = inp.IsMouseButtonDown(sf::Mouse::Left);
 		if (pressed) {
 			grid[mgy][mgx] = true;
+			dilagrid = dilate(grid, DILATION_AMOUNT);
 		}
 		bool pressedr = inp.IsMouseButtonDown(sf::Mouse::Right);
 		if (pressedr) {
 			grid[mgy][mgx] = false;
+			dilagrid = dilate(grid, DILATION_AMOUNT);
 		}
 
 		if (!(lastx == mgx && lasty == mgy) && !grid[mgy][mgx] && inp.IsKeyDown(sf::Key::Space)) {
-			boost::tie(l, p) = gridsearch(grid, gridvertex(mgx, mgy, &container));
+			boost::tie(l, p) = gridsearch(dilagrid, gridvertex(mgx, mgy, &container));
 			corners = get_turns(l.begin(), l.end());
 			lastx = mgx;
 			lasty = mgy;
@@ -89,6 +94,13 @@ int main()
 		}
 		for (int i = 0; i < wh; i += sz) {
 			App.Draw(sf::Shape::Line(0, i, ww-1, i, 1, sf::Color::Red));
+		}
+		for (int y = 0; y < gh; y++) {
+			for (int x = 0; x < gw; x++) {
+				if (dilagrid[y][x]) {
+					box(App, x, y, sf::Color(128, 128, 128));
+				}
+			}
 		}
 		for (int y = 0; y < gh; y++) {
 			for (int x = 0; x < gw; x++) {
